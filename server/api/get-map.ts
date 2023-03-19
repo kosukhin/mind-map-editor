6668dfd53bf04f7fba70eaf87912bec9f99e61b4
Eugene Kosukhin <kosuha606@gmail.com>
2023-03-19 16:05:42 +0300

    init

--- server/api/get-map.ts
+++ server/api/get-map.ts
@@ -1,12 +1,23 @@
 import fs from 'fs';
 import { baseHost, mapParamName } from "~/constants/server";
 
+const {existsSync, readFileSync} = fs;
+
 export default defineEventHandler((event) => {
   const {req} = event.node;
   const url = new URL(baseHost + req.url);
   const document = url.searchParams.get(mapParamName);
+  const filePath = `./maps/${document}.json`;
+  const fileExists = existsSync(filePath);
+  let data = {};
+
+  if (fileExists) {
+    data = JSON.parse(readFileSync(filePath).toString());
+  }
 
   return {
-    ok: true,
+    ok: fileExists,
+    data,
+    parentTypes: [],
   }
-})
+});
