6967c60510a2221003f7179539c928eef4c76704
Eugene Kosukhin <kosuha606@gmail.com>
2023-03-25 20:12:38 +0300

    maybe

--- components/view/Breadcrumbs/Breadcrumbs.vue
+++ components/view/Breadcrumbs/Breadcrumbs.vue
@@ -2,8 +2,25 @@
 import {useCurrentMap} from "~/composables";
 
 const {mapName} = useCurrentMap()
+let link = '';
+const mapHistory = computed(() => {
+  return mapName.split('/').map(history => {
+    link += '/' + history;
+    return {
+      link,
+      name: history,
+    };
+  });
+})
 </script>
 
 <template>
-  <div><a href="/">Главная</a> / {{ mapName }} </div>
+  <div>
+    <a href="/">Главная</a> /
+    <template v-for="history in mapHistory">
+      <a :href="history.link">
+        {{ history.name }}
+      </a> /
+    </template>
+  </div>
 </template>
