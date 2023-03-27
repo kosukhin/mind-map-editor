43c2ed24b9852fb6f846791a0c27a630ecdf2797
Eugene Kosukhin <githubkosuhin@gmail.com>
2023-03-27 15:38:05 +0300

    f

--- constants/system.ts
+++ constants/system.ts
@@ -1,3 +1,5 @@
 export const NOTIFY_DELAY = 2000;
 export const HEADER_HEIGHT = 50;
 export const SIDEBAR_WIDTH = 200;
+export const CANVAS_WIDTH = 1000;
+export const CANVAS_HEIGHT = 1000;
--- utils/konva/createLayer.ts
+++ utils/konva/createLayer.ts
@@ -1,5 +1,6 @@
 import Konva from "konva";
 import {MapLayer, MapStage} from "~/entities/MapLayer";
+import {CANVAS_HEIGHT, CANVAS_WIDTH} from "~/constants";
 
 const {Stage, Layer} = Konva;
 
@@ -14,6 +15,14 @@ export function createLayer(editorWrapper: HTMLElement): [MapLayer, MapStage] {
     fill: '#eee',
     draggable: true,
   });
+  stage.dragBoundFunc((pos) => {
+    const x = pos.x*-1;
+    const y = pos.y*-1;
+    return {
+      x: x < 0 ? 0 : x > CANVAS_WIDTH ? -1*CANVAS_WIDTH : pos.x,
+      y: y < 0 ? 0 : y > CANVAS_HEIGHT ? -1*CANVAS_HEIGHT : pos.y,
+    };
+  })
   const layer = new Layer();
   stage.add(layer);
   layer.draw();
