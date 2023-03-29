95233252078f8c98e77178feb8293494a0d9d30f
Eugene Kosukhin <githubkosuhin@gmail.com>
2023-03-29 16:27:25 +0300

    f

--- components/ui/Modal/Modal.vue
+++ components/ui/Modal/Modal.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import {watch} from "@vue/runtime-core";
-import { ref } from "@vue/reactivity";
+import {ref} from "@vue/reactivity";
 import {useOverlay} from "~/composables";
 
 const props = defineProps({
@@ -11,7 +11,7 @@ const props = defineProps({
 })
 
 const isOpened = ref(false);
-const {overlayName, tryToClose} = useOverlay();
+const {overlayName, tryToClose, history} = useOverlay();
 
 const close = () => {
   tryToClose.value = props.name;
@@ -22,11 +22,18 @@ watch(overlayName, () => {
     isOpened.value = vModal === props.name;
   })
 })
+
+const back = async () => {
+  history.value.pop();
+  overlayName.value = String(history.value.pop());
+}
 </script>
 
 <template>
   <div class="Modal" @click="close" v-if="isOpened">
     <div class="Modal-Inner" @click.stop>
+      <div v-if="history.length > 1" @click="back" title="Назад" class="Modal-Back">&lt;</div>
+      <div title="Закрыть" class="Modal-Close" @click="close">&times;</div>
       <div v-if="$slots.header" class="Modal-Header">
         <slot name="header" />
       </div>
--- composables/useOverlay.ts
+++ composables/useOverlay.ts
@@ -2,19 +2,32 @@ import {reactive, ref} from "@vue/reactivity";
 import {Maybe} from "~/entities";
 import {createSharedComposable} from "@vueuse/core";
 import {OVERLAY_CLOSE} from "~/constants";
+import {watch} from "@vue/runtime-core";
 
 export const useOverlay = createSharedComposable(() => {
   const overlayName = reactive(Maybe<string>())
   const tryToClose = reactive(Maybe<string>());
+  const history = ref<string[]>([]);
 
   const close = () => {
     overlayName.value = OVERLAY_CLOSE;
     tryToClose.value = OVERLAY_CLOSE;
   }
 
+  watch(overlayName, () => {
+    overlayName.map(vOverlay => {
+      if (vOverlay !== OVERLAY_CLOSE) {
+        history.value.push(vOverlay);
+      } else {
+        history.value = [];
+      }
+    })
+  });
+
   return {
     overlayName,
     tryToClose,
+    history,
     close,
   }
 })
