4288c222fabf4cf6370c40ef31ff0e91cc15dc2e
Eugene Kosukhin <githubkosuhin@gmail.com>
2023-04-27 10:47:25 +0300

    fix: Автоскрол при открытии ссылки с хэшем

--- composables/useHashChange.ts
+++ composables/useHashChange.ts
@@ -1,11 +1,24 @@
 import { createSharedComposable } from '@vueuse/core'
+import { watch } from '@vue/runtime-core'
 import { reMaybe } from '~/utils'
+import { useMap } from '~/composables'
 
 export const useHashChange = createSharedComposable(() => {
   const hashChanged = reMaybe<string>()
+  const { firstMapLoad } = useMap()
+
+  const getHashFromUrl = (url: string) => {
+    return url.split('#')[1] ?? null
+  }
 
   window.addEventListener('hashchange', (e) => {
-    hashChanged.value = e.newURL.split('#')[1] ?? ''
+    hashChanged.value = getHashFromUrl(e.newURL)
+  })
+
+  watch(firstMapLoad, () => {
+    setTimeout(() => {
+      hashChanged.value = getHashFromUrl(location.href)
+    })
   })
 
   return { hashChanged }
