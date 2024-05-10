cc0d1cd5bc62a9db73191de6b2abbf021b263980
Eugene Kosukhin <kosuha606@gmail.com>
2024-05-10 09:33:48 +0300

    feat(procedural): доделать тест на открытие меню

--- cypress/e2e/menu.cy.ts
+++ cypress/e2e/menu.cy.ts
@@ -3,6 +3,7 @@ describe('Menu', () => {
     cy.visit('http://localhost:8881/?view=/e2e/map.json');
     cy.get('.e2e-open-menu').click();
     cy.contains('Меню');
-    cy.get('.e2e-drawer-back').click();
+    cy.wait(1000).get('.e2e-drawer-back').click('topLeft');
+    cy.contains('Меню').should('not.exist');
   });
 });
