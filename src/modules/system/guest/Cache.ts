e6fb3a614e5196e839a4b0092e830e7ceac1b0f4
Eugene Kosukhin <kosuha606@gmail.com>
2024-08-31 20:47:29 +0300

    refactor(eo): Доработка кэш класса

--- src/modules/system/guest/Cache.ts
+++ src/modules/system/guest/Cache.ts
@@ -1,19 +1,14 @@
-import { GuestAwareType } from '@/modules/system/guest/GuestAwareType';
 import { PatronPool } from '@/modules/system/guest/PatronPool';
 import { CacheType } from '@/modules/system/guest/CacheType';
-import {
-  GuestType,
-  ReceiveOptions,
-} from './GuestType';
+import { GuestType, ReceiveOptions } from './GuestType';
 
-export class Cache<T> implements GuestType<T>, GuestAwareType<T>, CacheType<T> {
-  private pool: PatronPool<T>;
-
-  private theCache: T | null = null;
-
-  public constructor(initiator: unknown) {
-    this.pool = new PatronPool<T>(initiator);
-  }
+export class Cache<T> implements CacheType<T> {
+  public constructor(
+    private defaultValue: T,
+    initiator: unknown,
+    private theCache: T | null = null,
+    private pool = new PatronPool<T>(initiator),
+  ) {}
 
   public receive(value: T, options?: ReceiveOptions): this {
     this.theCache = value;
@@ -26,15 +21,6 @@ export class Cache<T> implements GuestType<T>, GuestAwareType<T>, CacheType<T> {
       guest.receive(this.theCache);
     }
     this.pool.add(guest);
-    return this;
-  }
-
-  public cache(guest: GuestType<T>, defaultValue: T): this {
-    if (this.theCache === null) {
-      guest.receive(defaultValue);
-    }
-    this.receiving(guest);
-
     return this;
   }
 }
--- src/modules/system/guest/CacheType.ts
+++ src/modules/system/guest/CacheType.ts
@@ -1,5 +1,4 @@
+import { GuestAwareType } from '@/modules/system/guest/GuestAwareType';
 import { GuestType } from './GuestType';
 
-export interface CacheType<T> {
-  cache(guest: GuestType<T>, defaultValue: T): this;
-}
+export type CacheType<T> = GuestType<T> & GuestAwareType<T>
