969e05ab15f2dc099fa6cfc7362f182d4691cbd5
Eugene Kosukhin <kosuha606@gmail.com>
2024-09-04 22:03:01 +0300

    feat(eo): map current test

---
+++ src/modules/application/mapFile/MapFileFake.ts
@@ -0,0 +1,34 @@
+import { MapFileType } from '@/modules/application/mapFile/MapFileType';
+import { MapFileDocument } from '@/modules/entities/MapStructures';
+import { PatronPool } from '@/modules/system/guest/PatronPool';
+
+export class MapFileFake implements MapFileType {
+  public constructor(
+    private mapFileDocument: MapFileDocument,
+    private currentMapPool = new PatronPool(this),
+    private mapFilePool = new PatronPool(this),
+  ) {}
+
+  public currentMap(target): this {
+    this.currentMapPool.distributeReceivingOnce(
+      this.mapFileDocument.current,
+      target,
+    );
+    return this;
+  }
+
+  public mapFile(target): this {
+    this.mapFilePool.distributeReceivingOnce(
+      this.mapFileDocument,
+      target,
+    );
+    return this;
+  }
+
+  public receive(value): this {
+    this.mapFileDocument = value;
+    this.currentMapPool.receive(value.current);
+    this.mapFilePool.receive(value);
+    return this;
+  }
+}
