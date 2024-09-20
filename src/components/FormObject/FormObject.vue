0e4f1fd69ea89f7232932af414bb92cb1b309675
Eugene Kosukhin <kosuha606@gmail.com>
2024-09-20 16:56:18 +0300

    feat(eo): вернул возможность редактировать объекты

--- src/components/FormObject/FormObject.vue
+++ src/components/FormObject/FormObject.vue
@@ -1,27 +1,192 @@
 <script lang="ts" setup>
 import BaseDrawer from '@/components/BaseDrawer/BaseDrawer.vue';
 import { useApplication } from '@/composables/useApplication';
-import { VueRefPatron } from '@/modules/integration/vue/VueRefPatron';
+import { VueComputedPatron } from '@/modules/integration/vue/VueComputedPatron';
+import { useFactories } from '@/composables/useFactories';
+import {
+  MapDocument,
+  MapObjectDocument,
+} from '@/modules/application/l1/l2/l3/map/documents/MapStructures';
+import BaseButton from '@/components/BaseButton/BaseButton.vue';
+import BaseCheckbox from '@/components/BaseCheckbox/BaseCheckbox.vue';
+import BaseInputTitle from '@/components/BaseInputTitle/BaseInputTitle.vue';
+import BaseInput from '@/components/BaseInput/BaseInput.vue';
+import BaseInputRow from '@/components/BaseInput/BaseInputRow.vue';
+import BaseEditor from '@/components/BaseEditor/BaseEditor.vue';
+import BaseSelect from '@/components/BaseSelect/BaseSelect.vue';
 
-const { mapObjectCurrent } = useApplication();
+const {
+  mapObjectCurrent, mapFile, mapObject, drawer,
+} = useApplication();
+const { patron, chain, guest } = useFactories();
 
-const objectId = mapObjectCurrent.objectId(new VueRefPatron<string>()).ref();
+type ObjectChainProps = {map: MapDocument, objectId: string};
+const object = new VueComputedPatron<MapObjectDocument>(() => {
+  const theChain = chain.create();
+  mapObjectCurrent.objectId(patron.create(theChain.receiveKey('objectId')));
+  mapFile.currentMap(patron.create(theChain.receiveKey('map')));
+  theChain.result(patron.create(
+    guest.create(({ map, objectId }: ObjectChainProps) => {
+      object.value = map.objects[objectId];
+    }),
+  ));
+}).ref();
+
+const remove = () => {
+  console.log('remove');
+};
 
 const close = () => {
   mapObjectCurrent.receive('');
+  drawer.receive('');
+};
+
+const save = () => {
+  mapObject.receive(object.value);
+  close();
 };
 </script>
 
 <template>
   <BaseDrawer name="object" @close="close">
     <template #header>
-      <h2 class="text-lg font-bold">Редактируем объект</h2>
+      <h2 class="text-lg font-bold">{{ $t('general.mapObject') }}</h2>
+      <small v-if="object" class="flex gap-2 items-center">
+        <span> ID #{{ object.id }} </span>
+        <BaseButton size="sm" type="primary">
+          {{ $t('general.copy') }}
+        </BaseButton>
+      </small>
     </template>
-    <div>
-      Тело формы объекта {{ objectId }}
+    <div v-if="object" class="flex flex-col gap-2">
+      <div class="FormObject-Inner">
+        <div class="FormObject-Row">
+          <BaseCheckbox
+            v-model="object.linked"
+            :label="$t('general.nameAsLink')"
+          />
+        </div>
+        <template v-if="object.linked">
+          <BaseInputTitle>{{ $t('general.outerLink') }}</BaseInputTitle>
+          <div class="FormObject-Row">
+            <BaseInput v-model="object.outlink" />
+          </div>
+          <div class="FormObject-Row">
+            <BaseCheckbox
+              v-model="object.targetBlank"
+              :label="$t('general.inNewTab')"
+            />
+          </div>
+        </template>
+        <BaseInputRow class="mb-2" :key="key" v-for="(_, key) in object.additionalFields">
+          <BaseInputTitle class="mb-1">
+            {{ key }}
+          </BaseInputTitle>
+          <BaseEditor v-model="object.additionalFields[key]" />
+        </BaseInputRow>
+        <BaseInputRow>
+          <BaseInputTitle>
+            {{ $t('general.topName') }}
+          </BaseInputTitle>
+          <BaseEditor v-model="object.additionalName" />
+        </BaseInputRow>
+        <BaseInputRow>
+          <BaseInputTitle>
+            {{ $t('general.bottomName') }}
+          </BaseInputTitle>
+          <BaseEditor v-model="object.name" />
+        </BaseInputRow>
+        <BaseInputRow>
+          <BaseInputTitle>
+            {{ $t('general.description') }}
+          </BaseInputTitle>
+          <BaseEditor v-model="object.description" />
+        </BaseInputRow>
+        <BaseInputRow>
+          <BaseInputTitle>
+            Z-Index
+          </BaseInputTitle>
+          <BaseInput v-model="object.zindex" type="number" />
+        </BaseInputRow>
+        <BaseInputRow>
+          <BaseInputTitle>
+            Width
+          </BaseInputTitle>
+          <BaseInput v-model="object.width" step="20" type="number" />
+        </BaseInputRow>
+        <BaseInputRow>
+          <BaseInputTitle>
+            Height
+          </BaseInputTitle>
+          <BaseInput v-model="object.height" step="20" type="number" />
+        </BaseInputRow>
+        <BaseInputRow>
+          <BaseInputTitle>
+            {{ $t('general.objectType') }}
+          </BaseInputTitle>
+          <BaseSelect
+            v-model="object.type"
+            :items="[]"
+            option-id="id"
+            option-label="name"
+          />
+        </BaseInputRow>
+        <div class="my-2">
+          <BaseButton
+            class="FormObject-ArrowButton"
+            type="primary"
+            size="md"
+          >
+            {{ $t('general.transfer') }}
+          </BaseButton>
+        </div>
+        <div class="my-2">
+          <BaseCheckbox
+            v-model="object.inMenu"
+            :label="$t('general.useInMenu')"
+          />
+        </div>
+        <template v-if="object.inMenu">
+          <div class="FormObject-Title">{{ $t('general.menuOrder') }}</div>
+          <div class="FormObject-Row">
+            <BaseInput v-model="object.menuOrder" type="number" />
+          </div>
+        </template>
+        <template v-if="object.arrows && object.arrows.length">
+          <div class="FormObject-Title">{{ $t('general.relations') }}</div>
+          <div class="FormObject-Row">
+            <div
+              v-for="(arrow) in object.arrows"
+              :key="arrow.id"
+              class="FormObject-Arrow"
+            >
+              <span class="FormObject-ArrowName">
+                obj
+              </span>
+              <BaseButton
+                class="FormObject-ArrowButton"
+                type="danger"
+                size="sm"
+              >
+                {{ $t('general.delete') }}
+              </BaseButton>
+            </div>
+          </div>
+        </template>
+      </div>
     </div>
     <template #footer>
-      кнопочки
+      <div class="py-3 flex gap-1">
+        <BaseButton type="success" @click="save">
+          {{ $t('general.save') }}
+        </BaseButton>
+        <BaseButton type="danger" @click="remove">
+          {{ $t('general.delete') }}
+        </BaseButton>
+        <BaseButton @click="close">
+          {{ $t('general.cancel') }}
+        </BaseButton>
+      </div>
     </template>
   </BaseDrawer>
 </template>
---
+++ src/modules/integration/vue/VueComputedPatron.ts
@@ -0,0 +1,17 @@
+import { ref, Ref } from 'vue';
+
+export class VueComputedPatron<T> {
+  private readonly innerRef: Ref<T | undefined>;
+
+  public constructor(
+    private executor: (theRef: Ref<T>) => void,
+    defaultValue: T | undefined = undefined,
+  ) {
+    this.innerRef = ref(defaultValue) as Ref<T>;
+  }
+
+  public ref<CT = undefined>(): Ref<CT extends undefined ? T : CT> {
+    this.executor(this.innerRef as Ref<T>);
+    return this.innerRef as Ref<CT extends undefined ? T : CT>;
+  }
+}
