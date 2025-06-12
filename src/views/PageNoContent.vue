366d751110c5812c7d71ba266be774c409635c04
Eugene Kosukhin <kosuha606@gmail.com>
2025-06-12 21:00:17 +0300

    feat(main): exclamation sign

--- src/views/PageNoContent.vue
+++ src/views/PageNoContent.vue
@@ -36,7 +36,7 @@ onMounted(() => {
   <div class="flex h-full w-full justify-center">
     <div class="flex flex-col text-center justify-center">
       <h3 class="text-h3 font-bold m-lg">
-        Нет данных для отображения
+        Нет данных для отображения!
       </h3>
       <p>
         Чтобы открыть схему установите приложение
